efc746e9cb52746b0f291a0b32b18827f75ac858
3asyice <guilhermeabreu294@gmail.com>
2025-11-18 13:40:52 -0300

    update

---
+++ script/js/birthday.js
@@ -0,0 +1,74 @@
+
+const anv = document.getElementById('birthday');
+
+const mes = new Date().getMonth();
+const dia = new Date().getDate();
+
+if (mes === 0 && dia === 27) {
+  anv.innerText = `Parabéns! Hoje é aniversário de Guilherme de Abreu Carvalho 🍿`;
+}
+/*
+  Janeiro   = 0
+  Fevereiro = 1
+  Março     = 2
+  Abril     = 3
+  Maio      = 4
+  Junho     = 5
+  Julho     = 6
+  Agosto    = 7
+  Setembro  = 8
+  Outubro   = 9
+  Novembro  = 10
+  Dezembro  = 11
+*/
+else if (mes === 8 && dia === 14)
+{
+anv.innerText = `Parabéns! Hoje é aniversário de Josiane Braga de Abreu❤️`;
+}
+else if (mes === 11 && dia === 5)
+{
+  anv.innerText = `Parabéns! Hoje é aniversário de Marisa Fernandes Braga❤️`;
+}
+else if (mes === 8 && dia === 22)
+{
+  anv.innerText = `Parabéns! Hoje é aniversário de Patrícia Braga de Abreu❤️`;
+}
+else if (mes === 1 && dia === 22)
+{
+  anv.innerText = `Parabéns! Hoje é aniversário de José Gabriel de Abreu dos Santos❤️`;
+}
+else if (mes === 5 && dia === 26)
+{
+  anv.innerText = `Parabéns! Hoje é aniversário de Maria Gabriela de Abreu dos Santos🎂`;
+}
+else if (mes === 10 && dia === 10)
+{
+  anv.innerText = `Parabéns! Hoje é aniversário de Camila Braga e Maethê Natasha🎂🎂`;
+}
+else if (mes === 10 && dia === 15)
+{
+  anv.innerText = `Parabéns! Hoje é aniversário de Thales Júnior🎂`;
+}
+else if (mes === 9 && dia === 28)
+{
+  anv.innerText = `Parabéns! Hoje é aniversário de Felipe Araújo 🎂`;
+}
+else if (mes === 4 && dia === 26)
+{
+  anv.innerText = `Parabéns! Hoje é aniversário de Cássia Araújo 🎂`;
+}
+else if (mes === 8 && dia === 23){  anv.innerText = `Parabéns! Hoje é aniversário de Odete Fernandes Braga🎂`;}
+else if (mes === 11 && dia === 24){  anv.innerText = `Parabéns! Hoje é aniversário de Regiane Braga Fernandes 🎂`;}
+else if (mes === 8 && dia === 28){  anv.innerText = `Parabéns! Hoje é aniversário de Cellanira Braga 🎂`;}
+else if (mes === 11 && dia === 17){  anv.innerText = `Parabéns! Hoje é aniversário de Abel Fernandes 🎂`;}
+else if (mes === 2 && dia === 26){  anv.innerText = `Parabéns! Hoje é aniversário de Kátia dos Santos Batista Fernandes🎂`;}
+else if (mes === 3 && dia === 30){  anv.innerText = `Parabéns! Hoje é aniversário de Katiele dos Santos Batista 🎂`;}
+else if (mes === 3 && dia === 9){  anv.innerText = `Parabéns! Hoje é aniversário de Diogo Batista 🎂`;}
+else if (mes === 11 && dia === 30){  anv.innerText = `Parabéns! Hoje é aniversário de Mirian Fernandes Braga 🎂`;}
+else if (mes === 4 && dia === 28){  anv.innerText = `Parabéns! Hoje é aniversário de Emilio dos Reis Carvalho 🎂`;}
+else if (mes === 6 && dia === 20){  anv.innerText = `Parabéns! Hoje é aniversário de Dagmar de Quadro Carvalho 🎂`;}
+else if (mes === 3 && dia === 24){  anv.innerText = `Parabéns! Hoje é aniversário de Vilma Trindade Carvalho 🎂`;}
+else if (mes === 3 && dia === 11){  anv.innerText = `Parabéns! Hoje é aniversário de Vera Azevedo Carvalho 🎂`;}
+else {
+  anv.innerText = `Hoje não tem nenhuma comemoração.`;
+}
