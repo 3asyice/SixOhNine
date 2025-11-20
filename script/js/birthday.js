ffa58421af37e10cf8636250b8eddbb035170f2f
3asyice <guilhermeabreu294@gmail.com>
2025-11-20 08:39:08 -0300

    update

--- script/js/birthday.js
+++ script/js/birthday.js
@@ -77,6 +77,7 @@ else if (mes === 1 && dia === 11){  anv.innerText = `Parabéns! Hoje é anivers�
 else if (mes === 2 && dia === 30){  anv.innerText = `Parabéns! Hoje é aniversário de Eliziane Saraiva Carvalho 🎂`;}
 else if (mes === 6 && dia === 31){  anv.innerText = `Parabéns! Hoje é aniversário de Josi Anselmini 🎂`;}
 else if (mes === 0 && dia === 28){  anv.innerText = `Parabéns! Hoje é aniversário de Daniel Carvalho 🎂`;}
+else if (mes === 5 && dia === 19){  anv.innerText = `Parabéns! Hoje é aniversário de José Carlos dos Reis Carvalho 🎂`;}
 else {
   anv.innerText = `Hoje não tem nenhuma comemoração.`;
 }
